274282cfb7a454a02afce13ba366cf3689b881ed
Anton Pavlov <toni.pavlov.85@bk.ru>
2022-08-14 18:24:24 +0300

    add an important func

--- ToDo/EntryViewController.swift
+++ ToDo/EntryViewController.swift
@@ -7,12 +7,25 @@
 
 import UIKit
 
-class EntryViewController: UIViewController {
+class EntryViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet var field: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        field.delegate
+        field.delegate = self
+    }
+    
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        saveTask()
+        
+        return true
+    }
+    @IBAction func saveTask() {
+        
+        
+        
     }
 }
