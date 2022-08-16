a1c210aea86e2fb03ee8bdf5132c0b7fe23acecc
Anton Pavlov <toni.pavlov.85@bk.ru>
2022-08-16 21:40:03 +0300

    create files in new VC

--- ToDo/TaskViewController.swift
+++ ToDo/TaskViewController.swift
@@ -9,7 +9,13 @@ import UIKit
 
 class TaskViewController: UIViewController {
     
+    @IBOutlet var label: UILabel!
+    
+    var task: String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        label.text = task
     }
 }
