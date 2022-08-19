e870d0b126cd696fc3ab1069f91826063802312b
Anton Pavlov <toni.pavlov.85@bk.ru>
2022-08-19 13:33:34 +0300

    delete some code

--- ToDo/ViewController.swift
+++ ToDo/ViewController.swift
@@ -66,7 +66,7 @@ extension ViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
         
-        let vc = storyboard?.instantiateViewController(withIdentifier: "task") as! TaskViewController
+        let vc = storyboard?.instantiateViewController(identifier: "task") as! TaskViewController
         vc.title = "Новая задача"
         vc.task = tasks[indexPath.row]
         navigationController?.pushViewController(vc, animated: true)
